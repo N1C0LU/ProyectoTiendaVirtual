5828eb36e5d7d3551daea4c35dc74d6b3386e9af
N1COLU <nicolas.castano@uao.edu.co>
2025-11-14 16:54:56 -0500

    Se inicio el proyecto de tienda virtual

---
+++ src/main/java/com/uao/tiendavirtualfinal/TiendaVirtualFinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uao.tiendavirtualfinal;
+
+/**
+ *
+ * @author Nicolas Castaño
+ */
+public class TiendaVirtualFinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
