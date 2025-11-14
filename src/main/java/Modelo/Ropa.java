2f802b376c5b3d625412e7b964e666a96eb6d70c
N1COLU <nicolas.castano@uao.edu.co>
2025-11-14 17:33:33 -0500

    Se creo el MVC, junto con la clase padre Producto

--- src/main/java/Modelo/Ropa.java
+++ src/main/java/Modelo/Ropa.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-package com.uao.tiendavirtualfinal;
+package Modelo;
 
 /**
  *
  * @author Nicolas Castaño
  */
-public class TiendaVirtualFinal {
-
-    public static void main(String[] args) {
-        
-    }
+public class Ropa {
+    
 }
