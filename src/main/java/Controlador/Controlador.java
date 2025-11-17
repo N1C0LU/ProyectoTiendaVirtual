09032bc651adb9a4611ca6a26f16f66efb69785c
scharris7 <scharris2007@gmail.com>
2025-11-17 18:54:17 -0500

    Se modifico la clase Controlador para agregar las imagenes

--- src/main/java/Controlador/Controlador.java
+++ src/main/java/Controlador/Controlador.java
@@ -13,11 +13,11 @@ public class Controlador {
     public ArrayList<Producto> obtenerProductos() {
         ArrayList<Producto> lista = new ArrayList<>();
 
-        lista.add(new Comida("Pizza", 25000));
-        lista.add(new Comida("Hamburguesa", 18000));
-        lista.add(new Electronico("Audífonos", 90000));
-        lista.add(new Electronico("Mouse Gamer", 65000));
-        lista.add(new Ropa("Camisa", 30000));
+        lista.add(new Comida("Pizza", 25000, "pizza.png"));
+        lista.add(new Comida("Hamburguesa", 18000, "hamburguesa.png"));
+        lista.add(new Electronico("Audífonos", 90000, "audifonos.png"));
+        lista.add(new Electronico("Mouse Gamer", 65000, "mouse.png"));
+        lista.add(new Ropa("Camisa", 30000, "camisa.png"));
 
         return lista;
     }
